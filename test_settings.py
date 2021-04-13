13fe63aaea57d5ce58d7e89cefbf5532b95a20fc
Jaroslav Martan <jmartan@cisco.com>
2021-04-13 20:52:11 +0200

    timestamp testing

--- test_settings.py
+++ test_settings.py
@@ -2,14 +2,15 @@ from unittest import TestCase
 from flask.testing import FlaskClient
 import os
 import json
+from datetime import datetime,timezone
 
 from ddb_single_table_obj import DDB_Single_Table
 ddb = DDB_Single_Table()
 print("initialize DDB object {}".format(ddb))
 
 settings_id = "Y2lzY29zcGFyazovL3VzL1BFT1BMRS82MzFlODQ0Mi02YTU3LTQ1ZTAtYjIyNy1jYWQ1Y2FkMmQ5MWQ"
-TEST_SETTINGS_1 = {"language": "cs_CZ", "partial_results": True, "active_votes": False, "user_1_1": False}
-TEST_SETTINGS_2 = {"language": "en_US", "partial_results": True, "active_votes": False, "user_1_1": False}
+TEST_SETTINGS_1 = {"language": "cs_CZ", "partial_results": True, "active_votes": False, "user_1_1": False, "user_updated": False, 'timestamp': '1970-01-01T01:00Z'}
+TEST_SETTINGS_2 = {"language": "en_US", "partial_results": True, "active_votes": False, "user_1_1": False, "user_updated": False, 'timestamp': '1970-01-01T01:00Z'}
 
 from settings import BotSettings
 
@@ -46,6 +47,11 @@ class BotTest(TestCase):
         stngs.load()
         self.assertEqual(stngs.stored, False)
         
+    def test_timestamp(self):
+        stngs = BotSettings()
+        self.assertEqual(stngs.timestamp, datetime.fromtimestamp(0).replace(tzinfo=timezone.utc))
+        stngs.save()
+        self.assertEqual(stngs.timestamp, datetime.fromtimestamp(stngs.timestamp.timestamp(), timezone.utc))
         
 if __name__ == "__main__":
     unittest.main()
